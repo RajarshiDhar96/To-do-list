bff59aa23ee848dd0255914d71bfce502cdbb58c
Rohan Dhar <rajarshid920@gmail.com>
2022-07-30 12:37:42 +0530

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express=require('express');
 const port=8000;
 const app=express();
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(req,res)
-{
-    return res.send('<h1>Hello World</h1>')
-})
+app.use(express.static('assets'));
+
+
+// app.get('/', function(req,res)
+// {
+//     return res.send('<h1>Hello World</h1>')
+// })
+
+app.use('/',require('./routes'))
 app.set('view engine','ejs');
 app.set('views','./views')
 app.listen(port,function(err)
