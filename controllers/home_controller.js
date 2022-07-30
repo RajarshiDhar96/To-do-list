d556912abb8d22cbb6ebd01135ccaa59f5ae626e
Rohan Dhar <rajarshid920@gmail.com>
2022-07-30 23:34:25 +0530

    populating the  database

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,32 @@
+const Task=require('../models/home.js')
 module.exports.home=function(req,res)
 {
     return res.render('home',{
         title:"To-Do-List"
     })
+}
+
+
+module.exports.createTask=function(req,res)
+{
+    console.log("I am inside create");
+    Task.create({
+        name:req.body.name,
+        date:req.body.date,
+        category:req.body.category
+
+    },
+    function(err, newTask)
+    {
+        if(err)
+        {
+            console.log("Error in creating contact");
+            return
+        }
+
+        console.log("********", newTask);
+        res.redirect("back");
+    }
+    
+    )
 }
