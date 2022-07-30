572e34250b9fb29cfdf2a5af7f508dbd1a14ee02
Rohan Dhar <rajarshid920@gmail.com>
2022-07-30 12:09:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const port=8000;
+const app=express();
+
+app.get('/', function(req,res)
+{
+    return res.send('<h1>Hello World</h1>')
+})
+app.set('view engine','ejs');
+app.set('views','./views')
+app.listen(port,function(err)
+{
+    if(err)
+    {
+        console.log("Error in running the server:", err);
+    }
+    console.log("Express server is running on the port: ", port);
+})
